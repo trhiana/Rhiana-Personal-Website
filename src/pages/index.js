1e80db84eedbd63594e6b61f70577683c7b343dd
trhiana <trhiana@rocketmail.com>
2020-07-30 16:28:16 +0300

    Updated index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import pic1 from '../assets/images/pic01.jpg';
 import pic4 from '../assets/images/pic04.png';
 import pic5 from '../assets/images/pic05.png';
 import pic6 from '../assets/images/pic06.png';
-import pic7 from '../assets/images/pic07.jpg';
+// import pic7 from '../assets/images/pic07.jpg';
 
 import config from '../../config';
 const IndexPage = () => (
@@ -46,58 +46,11 @@ const IndexPage = () => (
           </div>
         </div>
       </section>
-
-      {/* <section id="two" className="wrapper alt spotlight style2">
-        <div className="inner">
-          <a href="/#" className="image">
-            <img src={pic2} alt="" />
-          </a>
-          <div className="content">
-            <h2 className="major">Tempus adipiscing</h2>
-            <p>
-              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
-              turpis ante, nullam sit amet turpis non, sollicitudin posuere
-              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
-              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
-              cursus.
-            </p>
-            <a href="/#" className="special">
-              Learn more
-            </a>
-          </div>
-        </div>
-      </section>
-
-      <section id="three" className="wrapper spotlight style3">
-        <div className="inner">
-          <a href="/#" className="image">
-            <img src={pic3} alt="" />
-          </a>
-          <div className="content">
-            <h2 className="major">Nullam dignissim</h2>
-            <p>
-              Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras
-              turpis ante, nullam sit amet turpis non, sollicitudin posuere
-              urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim
-              dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla
-              cursus.
-            </p>
-            <a href="/#" className="special">
-              Learn more
-            </a>
-          </div>
-        </div>
-      </section> */}
+      
 
       <section id="four" className="wrapper alt style1">
         <div className="inner">
           <h2 className="major">Projects</h2>
-          {/* <p>
-            Cras mattis ante fermentum, malesuada neque vitae, eleifend erat.
-            Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis
-            egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus
-            in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.
-          </p> */}
           <section className="features">
             <article>
               <a href="/#" className="image">
@@ -133,19 +86,6 @@ const IndexPage = () => (
               </p>
               <a href="https://github.com/trhiana/trhiana.github.io" className="special" target="_blank" rel="noreferrer">
                 Check it out
-              </a>
-            </article>
-            <article>
-              <a href="/#" className="image">
-                <img src={pic7} alt="" />
-              </a>
-              <h3 className="major">Fusce consequat</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id
-                nulla dignissim dapibus ultrices.
-              </p>
-              <a href="/#" className="special">
-                Learn more
               </a>
             </article>
           </section>
